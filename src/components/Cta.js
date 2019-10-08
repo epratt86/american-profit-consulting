20d4c3a804d95a92e3863d180196673bfa9af16f
Eric Pratt <ericpratt86@gmail.com>
2019-10-07 17:45:37 -0700

    add background image

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -5,6 +5,7 @@ import styles from "../css/cta.module.css"
 const Cta = () => {
   return (
     <section className={styles.cta}>
+      <div className={styles.bgPattern}></div>
       <h3>
         need a fast, <span>business loan?</span>
       </h3>
@@ -13,6 +14,7 @@ const Cta = () => {
         to="/contact"
         className="btn-white"
         aria-label="Get a free quote"
+        id={styles.btn}
       >
         get a free quote
       </AniLink>
