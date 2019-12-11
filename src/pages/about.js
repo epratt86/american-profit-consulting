d89a540f71a7cfb28179c98608f65fe9698b3e04
Eric Pratt <ericpratt86@gmail.com>
2019-12-10 16:21:41 -0800

    source about from contentful

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,7 @@ import Numbers from "../components/Numbers"
 import SEO from "../components/SEO"
 import styles from "../css/about.module.css"
 import { useStaticQuery, graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Img from "gatsby-image"
 
 const getAbout = graphql`
@@ -25,12 +26,37 @@ const getAbout = graphql`
         }
       }
     }
+    content: allContentfulAbout {
+      edges {
+        node {
+          featuredImage {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+          textContent {
+            json
+          }
+        }
+      }
+    }
   }
 `
 
 const About = () => {
   // name must match name of query
-  const { aboutImage, aboutHero } = useStaticQuery(getAbout)
+  const { aboutImage, aboutHero, content } = useStaticQuery(getAbout)
+  const { featuredImage, textContent } = content.edges[0].node
+
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return <div></div>
+      },
+    },
+  }
+
+  console.log(textContent)
 
   return (
     <Layout>
@@ -46,38 +72,11 @@ const About = () => {
             <div className={styles.bio}>
               <article className={styles.aboutImg}>
                 <div className={styles.imgContainer}>
-                  <Img
-                    fluid={aboutImage.childImageSharp.fluid}
-                    alt="Humberto Hernandez"
-                  />
+                  <Img fluid={featuredImage.fluid} alt="Humberto Hernandez" />
                 </div>
               </article>
               <article className={styles.aboutInfo}>
-                <h4>Expertise you can rely on</h4>
-                <h5>
-                  Put over 15 years of financial services experience in your
-                  corner.
-                </h5>
-                <p>
-                  As a former business banker/lender, I have a deep passion and
-                  regard for business but have spent the last 14 years working
-                  for business banks under their agenda. During that time I knew
-                  I was under serving business owners around me because of the
-                  bias guidance I was forced to give in order to keep my job at
-                  the bank that employed me. Now I have the opportunity to guide
-                  and consult business owners with the TRUTH.
-                </p>
-                <h4>Insider Knowledge &amp; Pricing</h4>
-                <h5>
-                  Allow me to negotiate pricing on your behalf using the insider
-                  information to yield unprecedented savings.
-                </h5>
-                <p>
-                  With over 15 years of banking experience and having funded
-                  over $150,000,000 in business loans, I will use my expertise
-                  to navigate the entire loan process and negotiate excellent
-                  terms using the same information and knowledge that banks use.
-                </p>
+                {documentToReactComponents(textContent.json, options)}
                 <AniLink
                   fade
                   to="/contact"
